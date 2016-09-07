9a21b401075daca4c5cfc42c4e93167eade17199
Teresa M <teresawithoutah@gmail.com>
2016-09-07 14:53:20 -0400

    update
    restarted in jquery

--- ext/running.js
+++ ext/running.js
@@ -1,1 +1,29 @@
-$("body").append('Test');
+//ARRAY OF CENSOR WORDS
+var spoilers = ["today", "day", "run"];
+
+
+// Checking page title
+if (document.title.indexOf("Tumblr") != -1) {
+//if on tumblr print this
+console.log("ur on tumblr");
+
+//loop for searching page for elements in array
+var found = spoilers.every(function(word) {
+    return document.documentElement.innerText.indexOf(word) > -1;
+});
+
+//if exists print this
+if (found) { 
+console.log("spoilers found");
+$("p:contains('" + spoilers + "')").css("background-color", "yellow");
+} 
+
+
+
+
+
+
+
+}else{
+	console.log("ur not on tumblr");
+}
---
+++ ext/yea.js
@@ -0,0 +1,67 @@
+onsole.log("ur on tumblr");
+
+const words = ['mary', 'kill', 'bella']
+
+const p = document.querySelector('p')
+var newHTML = p.innerHTML
+
+$(function doIt() {
+    $('#changeColor').click( function() {
+       words.forEach(word=>
+		  newHTML = newHTML.replace(word, `<span class="color">${word}</span>`)
+		)
+		p.innerHTML = newHTML
+
+		result = newHTML
+	   console.log(result);
+		
+    });
+});
+var searchText = ["terrible", "sansa", "money"];
+var found;
+//search these elements
+var busca = document.getElementsByTagName("p");
+
+var found;
+
+var spoilers = ["sansa", "jon snow", "death"];
+var array = spoilers.length;
+
+
+//increments for page
+for (var i = 0; i < busca.length; i++) {
+  for (var j = 0; j < array; j++){
+    if (busca[i].textContent == [j] ) {
+  found = busca[i];
+  //if spoilers found 
+
+  
+  }
+}
+};
+      // if spoilers are on page
+if (found) {
+  //print the source class name containing the spoilers
+    console.log("spoiler exists!!");
+    console.log(found);
+
+//increments for page
+for (var i = 0; i < busca.length; i++) {
+  if (busca[i].textContent == searchText[]) {
+  found = busca[i];
+  //if spoilers found 
+
+  
+  }
+};
+      // if spoilers are on page
+if (found) {
+  //print the source class name containing the spoilers
+    console.log("spoiler exists!!");
+    console.log(found);
+
+}else {
+	console.log("spoiler not found");} 
+} else{
+	console.log("ur not on tumblr");
+}
