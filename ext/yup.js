38537440f2f47141d79471ac47c16d7b0586204e
Greg <gmelendrez98@gmail.com>
2016-11-06 11:00:45 -0500

    .

---
+++ ext/yup.js
@@ -0,0 +1,26 @@
+//ARRAY OF CENSOR WORDS
+var spoilers = ["today", "day", "run"];
+
+
+// Checking page title
+if (document.title.indexOf("Tumblr") != -1) {
+//if on tumblr print this
+console.log("ur on Facebook");
+
+//loop for searching page for elements in array
+var found = spoilers.every(function(word) {
+    return document.documentElement.innerText.indexOf(word) > -1;
+});
+
+//if exists print this
+if (found) { 
+console.log("spoilers found");
+$("p:contains('" + spoilers + "')").css("background-color", "yellow");
+} 
+
+
+
+
+}else{
+	console.log("ur not on Facebook");
+}
